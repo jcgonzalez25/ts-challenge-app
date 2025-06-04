6031c2a89f335767cf5229cc0df7c9c932e0d34d
JC Gonzalez <jc.gonzalez@perficient.com>
2025-06-04 14:17:42 -0500

    refactor: remove old formatter files from services directory

--- frontend/src/services/formatters/index.ts
+++
@@ -1,8 +0,0 @@
-// Main exports for the formatter service
-export * from './types';
-export * from './formatters';
-export * from './service';
-
-// Re-export for convenience
-export { FormatterService } from './service';
-export type { FieldFormatter, FormatterType, FormFormattersSchema } from './types'; 
